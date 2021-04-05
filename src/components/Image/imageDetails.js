fd19c173f3675bef925b947a61bdf3d2ec715efe
unknown <jahnavisananse@gmail.com>
2021-04-05 21:47:50 +0530

    formatting and css

--- src/components/Image/imageDetails.js
+++ src/components/Image/imageDetails.js
@@ -3,10 +3,17 @@ import React from 'react';
 import styles from '../Image/styles';
 import {StyleSheet, View, Text, Image, Button} from 'react-native';
 
-const ImageDetails = () => {
+const ImageDetails = props => {
   return (
     <Image
-      style={styles.tinyLogo}
+      // style={styles.tinyLogo}
+      style={{
+        marginTop: 20,
+        marginLeft: 20,
+        width: props.width,
+        height: props.height,
+        borderRadius: 10,
+      }}
       source={{
         uri: 'https://reactnative.dev/img/tiny_logo.png',
       }}
--- src/screens/demo.js
+++ src/screens/demo.js
@@ -1,5 +1,7 @@
 import React from 'react';
-// import * as images from '../images/images';
+import {WHATSAPP} from '../images/images';
+import * as images from '../images/images';
+
 import ImageDeatails from '../components/Image/imageDetails';
 import {StyleSheet, View, Text, Image, Button} from 'react-native';
 
@@ -14,8 +16,8 @@ const demo = () => {
           backgroundColor: 'yellow',
           marginTop: 10,
         }}>
-        {/* <Image style={styles.img} source={require('./like.png')} /> */}
-        <ImageDeatails />
+        <Image style={styles.img} source={WHATSAPP} />
+        <ImageDeatails width={'25%'} height={'75%'} />
         <Text style={{fontSize: 20, top: 40, left: 20}}> React-Native </Text>
       </View>
 
@@ -27,7 +29,8 @@ const demo = () => {
           backgroundColor: 'yellow',
           marginTop: 10,
         }}>
-        <ImageDeatails />
+        <Image style={styles.img} source={images.FACEBOOK} />
+        <ImageDeatails width={'25%'} height={'75%'} />
         <Text style={{fontSize: 20, top: 40, left: 20}}> Practice </Text>
       </View>
       <View
@@ -38,7 +41,9 @@ const demo = () => {
           backgroundColor: 'yellow',
           marginTop: 10,
         }}>
-        <ImageDeatails />
+        <Image style={styles.img} source={images.DOWNLOAD} />
+
+        <ImageDeatails width={'25%'} height={'75%'} />
         <Text style={{fontSize: 20, top: 40, left: 20}}> Schedule </Text>
       </View>
       <View
@@ -49,7 +54,9 @@ const demo = () => {
           backgroundColor: 'yellow',
           marginTop: 10,
         }}>
-        <ImageDeatails />
+        <Image style={styles.img} source={images.INSTAGRAM} />
+
+        <ImageDeatails width={'25%'} height={'75%'} />
         <Text style={{fontSize: 20, top: 40, left: 20}}> JAVA Script </Text>
       </View>
     </>
@@ -58,6 +65,13 @@ const demo = () => {
 
 const styles = StyleSheet.create({
   tinyLogo: {
+    marginTop: 20,
+    marginLeft: 20,
+    width: '25%',
+    height: '70%',
+    borderRadius: 10,
+  },
+  img: {
     marginTop: 20,
     marginLeft: 20,
     width: '25%',
